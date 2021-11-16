d60e01f9a9b4434e96057a1b9ba3221a905222ac
sree <sreekuttanpurachery@gmail.com>
2021-11-16 18:10:03 +0530

    Test Poc

---
+++ Project1/Script1.java
@@ -0,0 +1,58 @@
+
+import resources.Script1Helper;
+import com.rational.test.ft.*;
+import com.rational.test.ft.object.interfaces.*;
+import com.rational.test.ft.object.interfaces.SAP.*;
+import com.rational.test.ft.object.interfaces.WPF.*;
+import com.rational.test.ft.object.interfaces.dojo.*;
+import com.rational.test.ft.object.interfaces.siebel.*;
+import com.rational.test.ft.object.interfaces.flex.*;
+import com.rational.test.ft.object.interfaces.generichtmlsubdomain.*;
+import com.rational.test.ft.script.*;
+import com.rational.test.ft.value.*;
+import com.rational.test.ft.vp.*;
+import com.ibm.rational.test.ft.object.interfaces.sapwebportal.*;
+/**
+ * Description   : Functional Test Script
+ * @author admin
+ */
+public class Script1 extends Script1Helper
+{
+	/**
+	 * Script Name   : <b>Script1</b>
+	 * Generated     : <b>Nov 16, 2021 5:53:32 PM</b>
+	 * Description   : Functional Test Script
+	 * Original Host : WinNT Version 6.3  Build 9600 ()
+	 * 
+	 * @since  2021/11/16
+	 * @author admin
+	 */
+	public void testMain(Object[] args) 
+	{
+		startApp("ClassicsJavaA");
+		
+		// Frame: ClassicsCD
+		tree2().click(atPath("Composers->Schubert->Location(PLUS_MINUS)"));
+		tree2().click(atPath("Composers->Schubert->String Quartets Nos. 4 & 14"));
+		placeOrder().click();
+		
+		// Frame: Member Logon
+		password().click(atPoint(21,15));
+		memberLogon().inputChars("1234");
+		ok().click();
+		
+		// Frame: Place an Order
+		cardNumberIncludeTheSpaces().click(atPoint(25,18));
+		placeAnOrder().inputChars("111111111111111");
+		expirationDate().click(atPoint(21,10));
+		placeAnOrder().inputChars("04/02");
+		placeOrder2().click();
+		
+		// 
+		ok2().click();
+		
+		// Frame: ClassicsCD
+		classicsCD(ANY,MAY_EXIT).maximize();
+	}
+}
+
