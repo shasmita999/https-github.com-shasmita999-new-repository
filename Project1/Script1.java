f73c8bc6fdbe91f21a3337fc61b8a0c5c6870513
sree <sreekuttanpurachery@gmail.com>
2021-11-17 07:13:03 +0530

    Poc

--- Project1/Script1.java
+++ Project1/Script1.java
@@ -38,7 +38,7 @@ public class Script1 extends Script1Helper
 		
 		// Frame: Member Logon
 		password().click(atPoint(21,15));
-		memberLogon().inputChars("123456");
+		memberLogon().inputChars("12");
 		ok().click();
 		
 		// Frame: Place an Order
