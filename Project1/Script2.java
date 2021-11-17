77eb5dfea531566475c05a1beaa47c7ca246ae38
sree <sreekuttanpurachery@gmail.com>
2021-11-17 14:46:04 +0530

    add script 2

---
+++ Project1/Script2.java
@@ -0,0 +1,41 @@
+
+import resources.Script2Helper;
+import com.rational.test.ft.*;
+import com.rational.test.ft.object.interfaces.*;
+import com.rational.test.ft.object.interfaces.SAP.*;
+import com.rational.test.ft.object.interfaces.WPF.*;
+import com.rational.test.ft.object.interfaces.dojo.*;
+import com.rational.test.ft.object.interfaces.siebel.*;
+import com.rational.test.ft.object.interfaces.flex.*;
+import com.rational.test.ft.object.interfaces.generichtmlsubdomain.*;
+import com.rational.test.ft.script.*;
+import com.rational.test.ft.value.*;
+import com.rational.test.ft.vp.*;
+import com.ibm.rational.test.ft.object.interfaces.sapwebportal.*;
+/**
+ * Description   : Functional Test Script
+ * @author admin
+ */
+public class Script2 extends Script2Helper
+{
+	/**
+	 * Script Name   : <b>Script2</b>
+	 * Generated     : <b>Nov 17, 2021 2:43:42 PM</b>
+	 * Description   : Functional Test Script
+	 * Original Host : WinNT Version 6.3  Build 9600 ()
+	 * 
+	 * @since  2021/11/17
+	 * @author admin
+	 */
+	public void testMain(Object[] args) 
+	{
+		startApp("application");
+		
+		// HTML Browser
+		browser_htmlBrowser(document_google(),DEFAULT_FLAGS).close();
+		
+		
+		
+	}
+}
+
